205260d7079185d8aaa980abdecd622f5449bd20
Travis Rasor <travis.rasor@argodata.com>
2014-12-16 21:28:16 -0600

    More plugins

--- aliases.sh
+++ aliases.sh
@@ -2,6 +2,10 @@
 alias mkdir="mkdir -p "
 alias ll="ls -lShra "
 alias lt="ls -lthra "
+alias grep='grep --color'
+alias mv='mv -i'
+alias cp='cp -i'
+alias rm='rm -i'
 
 alias ping_google="ping 8.8.8.8"
 alias external_ip='curl ifconfig.me'
--- aliases.system.sh
+++ aliases.system.sh
@@ -3,6 +3,9 @@ alias zcustom="vim $ZSH_CUSTOM/custom.sh"
 alias zalias="vim $ZSH_CUSTOM/aliases.sh"
 alias zfunctions="vim $ZSH_CUSTOM/functions.sh"
 alias zshconfig="subl $ZSH_CUSTOM"
+# Make zsh know about hosts already accessed by SSH
+zstyle -e ':completion:*:(ssh|scp|sftp|rsh|rsync):hosts' hosts 'reply=(${=${${(f)"$(cat {/etc/ssh_,~/.ssh/known_}hosts(|2)(N) /dev/null)"}%%[# ]*}//,/ })'
+
 
 alias df="df -h "
 alias diskusage='du -hd1'
