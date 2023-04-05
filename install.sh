2faf13f42ddd7497451d1fdf45b0a3fa05ba698c
Travis Rasor <travis@thathanka.org>
2023-04-05 10:38:10 -0500

    making install script easier

--- install.sh
+++ install.sh
@@ -1,5 +1,13 @@
 #!/usr/bin/env bash
 
+REPO_DIR="$(dirname -- "${BASH_SOURCE[0]}")"            # relative
+REPO_DIR="$(cd -- "$REPO_DIR" && pwd)"    # absolutized and normalized
+if [[ -z "$REPO_DIR" ]] ; then
+  # error; for some reason, the path is not accessible
+  # to the script (e.g. permissions re-evaled after suid)
+  exit 1  # fail
+fi
+
 if [[ $OSTYPE == 'darwin'* ]]; then
     source "./install-macos-software.sh"
 elif [[ $OSTYPE == 'linux'* ]]; then
@@ -9,11 +17,6 @@ else
     exit 1
 fi
 
-#setup antigen and local profile
-echo "Creating Repos at ${HOME}/Repos"
-mkdir -p "${HOME}/Repos"
-REPOS_DIR="${HOME}/Repos"
-
 
 # Install antigen
 if ! [ -f "${HOME}/antigen.zsh" ]; then
@@ -22,7 +25,7 @@ fi
 
 
 git clone https://github.com/tlrasor/zshrc.git
-ln -s "$REPOS_DIR/zshrc/zshrc.zsh" "${HOME}/.zshrc"
+ln -s "$REPO_DIR/zshrc/zshrc.zsh" "${HOME}/.zshrc"
 
 
 #install sdkman
